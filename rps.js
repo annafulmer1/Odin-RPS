57194c18a77d728a9715ea92d6cfed11600801cd
annafulmer1 <anna.r.fulmer@gmail.com>
2022-06-28 15:54:38 -0500

    Game Function to Play for Five Rounds
    Added game function to play for a total of five rounds and report the winner!

--- rps.js
+++ rps.js
@@ -1,47 +1,83 @@
 "use strict";
 //User Player Input
-let playerChoice = prompt("which do you choose?", "");
-let playerSelectionFixed = playerChoice.toLowerCase();
+let computerScore = 0;
+let userScore = 0;
 
-//Computer Player Function
-let trial = Math.trunc(Math.random() * 3) + 1;
-let computerResult;
-let computerPlay = function (num) {
-  if (trial == 1) {
-    return (computerResult = "rock");
-  } else if (trial == 2) {
-    return (computerResult = "paper");
+//check winner
+const checkWinner = function () {
+  if (userScore > computerScore) {
+    return " User wins!";
+  } else if (userScore < computerScore) {
+    return "computer wins :(";
   } else {
-    return (computerResult = "scissors");
+    return "it's a tie!";
   }
 };
-computerPlay();
 
-//Double check outputs
-console.log(
-  `I pick ${playerSelectionFixed} + computer picks ${computerResult}`
-);
+//game function
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    let playerChoice = prompt("which do you choose?", "");
+    let playerSelectionFixed = playerChoice.toLowerCase();
 
-//Play a single Round Game vs Computer
-let computerScore;
-let userScore;
-let singleRound = function (playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) {
-    return "It's a Draw!";
-  } else if (playerSelection == "rock" && computerSelection == "paper") {
-    return "You lose!";
-  } else if (playerSelection == "rock" && computerSelection == "scissors") {
-    return "You win!";
-  } else if (playerSelection == "paper" && computerSelection == "scissors") {
-    return "You lose!";
-  } else if (playerSelection == "paper" && computerSelection == "rock") {
-    return "You win!";
-  } else if (playerSelection == "scissors" && computerSelection == "paper") {
-    return "You win!";
-  } else if (playerSelection == "scissors" && computerSelection == "rock") {
-    return "You lose!";
+    //Computer Player Function
+    let trial = Math.trunc(Math.random() * 3) + 1;
+    let computerResult;
+    let computerPlay = function (num) {
+      if (trial == 1) {
+        return (computerResult = "rock");
+      } else if (trial == 2) {
+        return (computerResult = "paper");
+      } else {
+        return (computerResult = "scissors");
+      }
+    };
+    computerPlay();
+
+    //Double check outputs
+    console.log(
+      `I pick ${playerSelectionFixed} + computer picks ${computerResult}`
+    );
+
+    //Play a single Round Game vs Computer
+
+    let singleRound = function (playerSelection, computerSelection) {
+      if (playerSelection == computerSelection) {
+        return "It's a Draw!";
+      } else if (playerSelection == "rock" && computerSelection == "paper") {
+        computerScore++;
+        return "You lose!";
+      } else if (playerSelection == "rock" && computerSelection == "scissors") {
+        userScore++;
+        return "You win!";
+      } else if (
+        playerSelection == "paper" &&
+        computerSelection == "scissors"
+      ) {
+        computerScore++;
+        return "You lose!";
+      } else if (playerSelection == "paper" && computerSelection == "rock") {
+        userScore++;
+        return "You win!";
+      } else if (
+        playerSelection == "scissors" &&
+        computerSelection == "paper"
+      ) {
+        userScore++;
+        return "You win!";
+      } else if (playerSelection == "scissors" && computerSelection == "rock") {
+        computerScore++;
+        return "You lose!";
+      } else {
+        return "not a valid selection";
+      }
+    };
+    singleRound(playerSelectionFixed, computerResult);
+
+    console.log(
+      `Round ${i} of 5.  Your score is ${userScore} and the computer score is ${computerScore}!`
+    );
   }
-};
-console.log(singleRound(playerSelectionFixed, computerResult));
-
-let game = function () {};
+}
+game();
+console.log(checkWinner());
